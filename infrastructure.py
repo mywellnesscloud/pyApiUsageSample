095ba18620fcbaff8a5a546456cc1d8f01114da6
Carlo Zamagni <cazamagni@technogym.com>
2014-09-02 09:32:25 +0200

    changed app id

--- infrastructure.py
+++ infrastructure.py
@@ -1,5 +1,5 @@
 __author__ = 'cazamagni'
 
 
-my_wellness_api_app_id = 'carloappsample'
+my_wellness_api_app_id = 'githubwellnessapi'
 my_wellness_api_app_secret = '06853AEC62BFF62A7270701A3C438048DFDF2203'
