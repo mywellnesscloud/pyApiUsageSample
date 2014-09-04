128a9b3fc87dff57442b138bf2f6491aa92688cb
Carlo Zamagni <cazamagni@technogym.com>
2014-09-04 11:31:04 +0200

    time series

--- infrastructure.py
+++ infrastructure.py
@@ -3,9 +3,11 @@ __author__ = 'cazamagni'
 
 my_wellness_api_app_id = 'githubwellnessapi'
 my_wellness_api_app_secret = '06853AEC62BFF62A7270701A3C438048DFDF2203'
-
+'''
+my_wellness_api_app_id = 'carloappsample'
+my_wellness_api_app_secret = '06853AEC62BFF62A7270701A3C438048DFDF2203'
+'''
 my_wellness_api_base_url = 'https://servicestestext.mywellness.com'
-#my_wellness_api_base_url = 'https://apilocal.mywellness.com'
 my_wellness_api_auth_base_url = 'https://usertestext.mywellness.com:13443'
 
 verify_ssl_cert = False
--- routes/activities/activities.py
+++ routes/activities/activities.py
@@ -7,8 +7,42 @@ from flask import Blueprint, redirect, request, render_template, abort, session
 
 __author__ = 'carlozamagni'
 
-
 activities_app = Blueprint('activities', __name__, static_folder='static', template_folder='templates')
+
+requests_headers = {'Content-Type': 'application/json',
+                    'X-MWAPPS-OAUTHCLIENTID': infrastructure.my_wellness_api_app_id}
+
+
+@activities_app.route('/timeseries/')
+@activities_app.route('/timeseries/<data_type>')
+def time_series(data_type=''):
+    user_id = session.get('user_id', None)
+    user_token = session.get('user_token', None)
+
+    if user_id is None or user_token is None:
+        # go and authorize before uploading
+        return redirect('/authorize')
+
+    if len(data_type) == 0 or data_type not in ['Move', 'Calories', 'RunningDistance', 'CyclingDistance']:
+        return render_template('/activities/time_series.html')
+
+    time_series_url = '%s/api/v1/ActivityStream/%s/TimeSeries' % (infrastructure.my_wellness_api_base_url, '201408')
+    response = requests.get(url=time_series_url,
+                            data=json.dumps({'dataType': data_type, 'token': user_token}),
+                            headers=requests_headers)
+
+    if response.status_code != 200:
+        render_template('/activities/time_series.html')
+
+    response_data = (json.loads(response.content))['data']
+
+    labels = []
+    data = []
+    for d in response_data['days']:
+        labels.append(str(d['day'])[-2:len(str(d['day']))])
+        data.append(int(d['value']))
+
+    return render_template('/activities/time_series.html', labels=json.dumps(labels), series_data=json.dumps(data))
 
 
 @activities_app.route('/upload', methods=['GET', 'POST'])
@@ -32,8 +66,7 @@ def activity_upload():
 
         res = requests.post(url=url,
                             data=json.dumps(payload),
-                            headers={'Content-Type': 'application/json',
-                                     'X-MWAPPS-OAUTHCLIENTID': infrastructure.my_wellness_api_app_id},
+                            headers=requests_headers,
                             verify=infrastructure.verify_ssl_cert)
 
         print(res.content)
@@ -47,3 +80,14 @@ def activity_upload():
 def _is_valid_file(file):
     u_file_name = file.filename.upper()
     return '.FIT' in u_file_name or '.TCX' in u_file_name
+
+
+def _get_current_user_or_login():
+    user_id = session.get('user_id', None)
+    user_token = session.get('user_token', None)
+
+    if user_id is None or user_token is None:
+        # go and authorize before uploading
+        return redirect('/authorize')
+
+    return user_id, user_token
