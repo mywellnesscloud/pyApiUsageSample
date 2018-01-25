5b54bcb4c1791c9fe4df52991b02261a6cd29faf
Carlo Zamagni <cazamagni@technogym.com>
2018-01-25 09:49:20 +0100

    base test URI updated

--- infrastructure.py
+++ infrastructure.py
@@ -7,7 +7,7 @@ my_wellness_api_app_secret = '06853AEC62BFF62A7270701A3C438048DFDF2203'
 my_wellness_api_app_id = 'carloappsample'
 my_wellness_api_app_secret = '06853AEC62BFF62A7270701A3C438048DFDF2203'
 '''
-my_wellness_api_base_url = 'https://servicestestext.mywellness.com'
+my_wellness_api_base_url = 'https://services-test.mywellness.com'
 my_wellness_api_auth_base_url = 'https://usertestext.mywellness.com:13443'
 
 verify_ssl_cert = False
